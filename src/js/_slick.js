a994576cc742c024573ca02cb00cfd6980dcdac7
codajoker <codajoker91@gmail.com>
2021-11-27 18:42:08 +0200

    1

--- src/js/_slick.js
+++ src/js/_slick.js
@@ -60,7 +60,7 @@ $(document).ready(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 2500,
     speed: 1000,
     fade: true,
 
