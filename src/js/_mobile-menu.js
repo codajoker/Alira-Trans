0310809fe81affc76c54d1d9902ae8353505da52
codajoker <codajoker91@gmail.com>
2021-11-27 16:01:56 +0200

    1

--- src/js/_mobile-menu.js
+++ src/js/_mobile-menu.js
@@ -9,6 +9,7 @@ refs.burgerBtn.addEventListener('click', onButtonClick);
 refs.closeBtn.addEventListener('click', onButtonClick);
 
 function onButtonClick(e) {
+  document.body.classList.toggle('bg-scrolling-element-when-mobile-open');
   refs.backdrop.classList.toggle('is-hidden');
   refs.mobileMenu.classList.toggle('mobile-open');
 }
--- src/js/_modal.js
+++ src/js/_modal.js
@@ -10,12 +10,14 @@ refs.openModalBtn.forEach(button => button.addEventListener('click', openModal))
 refs.closeModalBtn.addEventListener('click', closeModal);
 
 function openModal() {
+  document.body.classList.add('bg-scrolling-element-when-mobile-open');
   refs.mobileMenu.classList.remove('mobile-open');
   refs.backdrop.classList.remove('is-hidden');
   refs.modal.classList.remove('is-hidden');
 }
 
 function closeModal() {
+  document.body.classList.remove('bg-scrolling-element-when-mobile-open');
   refs.backdrop.classList.add('is-hidden');
   refs.modal.classList.add('is-hidden');
 }
