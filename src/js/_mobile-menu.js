44c761ecca390353c33525f28996b547040a0e30
codajoker <codajoker91@gmail.com>
2021-11-27 16:14:48 +0200

    1

--- src/js/_mobile-menu.js
+++ src/js/_mobile-menu.js
@@ -3,11 +3,12 @@ const refs = {
   closeBtn: document.querySelector('#mobile-close'),
   mobileMenu: document.querySelector('#mobile-menu'),
   backdrop: document.querySelector('#backdrop'),
+  modalLink: document.querySelectorAll('.mobile-nav__link'),
 };
-
+console.log(refs.modalLink);
 refs.burgerBtn.addEventListener('click', onButtonClick);
 refs.closeBtn.addEventListener('click', onButtonClick);
-
+refs.modalLink.forEach(link => link.addEventListener('click', onButtonClick));
 function onButtonClick(e) {
   document.body.classList.toggle('bg-scrolling-element-when-mobile-open');
   refs.backdrop.classList.toggle('is-hidden');
